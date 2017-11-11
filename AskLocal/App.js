d7d1fc54f4070c01a4c829b5220c14a319d43a85
Philipp Klocke <Phil_K97@gmx.de>
2017-11-12 07:12:17 +0800

    Fixed Font, hide Android/iOS status

--- AskLocal/App.js
+++ AskLocal/App.js
@@ -1,10 +1,35 @@
-import React, {Component} from 'react';
+import Expo from "expo";
+import React from "react";
+import { StatusBar, Container } from "react-native";
 
 import Main from './src/components/Main';
+
 export default class App extends React.Component {
-  render() {
-    return (
-		<Main/>
-);
-  }
+	constructor() {
+	super();
+		this.state = {
+			isReady: false
+		};
+	}
+
+	async componentWillMount() {
+		await Expo.Font.loadAsync({
+		Roboto: require("native-base/Fonts/Roboto.ttf"),
+		Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+		Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+	});
+
+		this.setState({ isReady: true });
+	}
+
+	componentDidMount() {
+		StatusBar.setHidden(true);
+	}
+
+	render() {
+		if (!this.state.isReady) {
+			return <Expo.AppLoading />;
+		}
+		return <Main />;
+	}
 }
