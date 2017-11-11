fc9a7bf6baacb7ab9c53614fa12eb379abe34a5b
Philipp Klocke <Phil_K97@gmx.de>
2017-11-12 05:28:14 +0800

    Added Profile, Fixed Ask action button

--- AskLocal/src/components/Ask.js
+++ AskLocal/src/components/Ask.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { View, Text, Content, List, ListItem } from 'native-base';
-import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class Ask extends React.Component {
 	render() {
-		var items = ['Simon Mignolet','Nathaniel Clyne','Dejan Lovren','Mama Sakho','Emre Can'];
+		var items = [];
+		for(var i = 0; i < 50; i++) {
+			items.push(`blabla number ${i}`);
+		}
 		return (
 			<Content>
 			<View>
@@ -18,17 +19,8 @@ export default class Ask extends React.Component {
 			}>
 			</List>
 			</View>
-			<ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => { console.log("hi")}}>
-			</ActionButton>
 			</Content>
 		);
 	}
 }
 
-const styles = StyleSheet.create({
-	actionButtonIcon: {
-		fontSize: 20,
-		height: 22,
-		color: 'white',
-	},
-});
--- AskLocal/src/components/Main.js
+++ AskLocal/src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Content, Tab, Tabs } from 'native-base';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Ask from './Ask';
 import Answer from './Answer';
+import Profile from './Profile';
 
 export default class Main extends React.Component {
 	render() {
@@ -12,10 +15,13 @@ export default class Main extends React.Component {
 		<Header hasTabs />
 		<Tabs initialPage={1}>
 			<Tab heading="Profile">
+				<Profile/>
 			</Tab>
 			<Tab heading="Ask">
 				<Text> I ASK QUESTIONS </Text>
 				<Ask/>
+				<ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => { console.log("hi")}}>
+				</ActionButton>
 			</Tab>
 			<Tab heading="Answer">
 				<Text> I ANSWER QUESTIONS </Text>
@@ -25,3 +31,11 @@ export default class Main extends React.Component {
     </Container>
 	);
 }}
+
+const styles = StyleSheet.create({
+	actionButtonIcon: {
+		fontSize: 20,
+		height: 22,
+		color: 'white',
+	},
+});
