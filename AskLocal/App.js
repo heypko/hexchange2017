8de8aa73c1bc88773205157b9e361ba06e79b88b
Tim Pfeifle <pfeifletim@gmail.com>
2017-11-12 02:49:22 +0800

    add ui framework

--- AskLocal/App.js
+++ AskLocal/App.js
@@ -5,9 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={{flex:1}}>Open up App.js to start working on your app!</Text>
+        <Text style={{flex:2}}>Changes you make will automatically reload.</Text>
+        <Text >Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
@@ -16,7 +16,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
