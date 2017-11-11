288360dc0719990ec83ff5eb50e8dac923e9c82c
Philipp Klocke <Phil_K97@gmx.de>
2017-11-12 05:09:07 +0800

    Moved action button to ask

--- AskLocal/src/components/Answer.js
+++ AskLocal/src/components/Answer.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
-
+import { View, Text } from 'react-native';
 
 export default class Answer extends React.Component {
 	render() {
 		return (
 			<View style={{flex:1, backgroundColor: '#f3f3f3'}}>
 			<Text> MockAnswer </Text>
-			{/* Rest of the app comes ABOVE the action button component !*/}
-			<ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => { console.log("hi")}}>
-
-			</ActionButton>
 			</View>
 		);
 	}
 }
 
-const styles = StyleSheet.create({
-	actionButtonIcon: {
-		fontSize: 20,
-		height: 22,
-		color: 'white',
-	},
-});
--- AskLocal/src/components/Ask.js
+++ AskLocal/src/components/Ask.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { View, Text, Content, List, ListItem } from 'native-base';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class Ask extends React.Component {
 	render() {
@@ -15,7 +18,17 @@ export default class Ask extends React.Component {
 			}>
 			</List>
 			</View>
+			<ActionButton buttonColor="rgba(231,76,60,1)" onPress={() => { console.log("hi")}}>
+			</ActionButton>
 			</Content>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	actionButtonIcon: {
+		fontSize: 20,
+		height: 22,
+		color: 'white',
+	},
+});
